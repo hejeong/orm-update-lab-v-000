3545111f225047276df84da082889e202dd3ecf7
Jonathan Hong <jonhong777@gmail.com>
2018-08-05 04:57:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,5 +27,6 @@ class Student
           VALUES (?,?)
           SQL
     DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
   end
 end
