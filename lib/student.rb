e10067830a4839a30b100441e69d14e4f86b207e
Jonathan Hong <jonhong777@gmail.com>
2018-08-05 05:11:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,6 +36,8 @@ class Student
   # create instances
   def self.create(name, grade)
     new_student = Student.new(name, grade)
+    new_student.save
+    new_student
   end
 
   # <-- instance methods
